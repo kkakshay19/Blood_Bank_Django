51c0665cf4a82f03585532f4f36fd5731f8d3174
kkakshay19 <kkakshaymanoj19@gmail.com>
2025-10-14 11:31:10 +0530

    Updated feature X / fixed bug / added mysqldb

---
+++ mywebsite/__init__.py
@@ -0,0 +1,4 @@
+import pymysql
+
+# Allow Django to use PyMySQL in place of MySQLdb
+pymysql.install_as_MySQLdb()
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bloodbank',
+        'USER': 'root',
+        'PASSWORD': 'Akshay@125*',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
